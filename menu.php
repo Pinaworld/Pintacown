065ee2a0826c5bd266100807a3c7dd6e88117bdc
Pinaworld <Pinben02@gmail.com>
2015-03-02 10:19:14 +0100

    Pinactown  (Benoit Pinault)

--- menu.php
+++ menu.php
@@ -8,19 +8,20 @@
 <html><head>
 
 
-    <script src="bootstrap/jquery-1.11.2.min.js"></script>
-
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
 
-    <script src="bootstrap/bootstrap.min.js"></script>
-
-
-    <!--<meta name="viewport" content="width=device-width" />
+    <meta name="viewport" content="width=device-width" />
     <LINK href="style/bootstrap.css" rel="stylesheet" type="text/css">
-    <link href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" />-->
 
+    <title>Hello - Bootstrap-submenu</title>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+
+    <script src="bootstrap/bower_components/jquery/dist/jquery.js" defer="defer"></script>
+    <script src="bootstrap/bower_components/bootstrap/dist/js/bootstrap.js" defer="defer"></script>
+    <script src="bootstrap/bootstrap-submenu.min.js" defer="defer"></script>
+    <script src="bootstrap/docs.min.js" defer="defer"></script>
 
 </head>
 
@@ -30,22 +31,129 @@
 
 <script src="bootstrap/bootstrap.min.js"></script>
 
+<!--
+<nav class="navbar navbar-default">
+    <div class="navbar-header">
+        <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-collapse"><span
+                class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span
+                class="icon-bar"></span> <span class="icon-bar"></span></button>
+        <a class="navbar-brand">Project Name</a></div>
+    <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+            <li class="dropdown"><a tabindex="0" data-toggle="dropdown">Dropdown<span class="caret"></span></a>
+                <ul class="dropdown-menu" role="menu">
+                    <li class="dropdown-submenu"><a tabindex="0" data-toggle="dropdown">Action</a>
+                        <ul class="dropdown-menu">
+                            <li><a tabindex="0">Sub action</a></li>
+                            <li class="dropdown-submenu"><a tabindex="0" data-toggle="dropdown">Another sub
+                                    action</a>
+                                <ul class="dropdown-menu">
+                                    <li><a tabindex="0">Sub action</a></li>
+                                    <li><a tabindex="0">Another sub action</a></li>
+                                    <li><a tabindex="0">Something else here</a></li>
+                                </ul>
+                            </li>
+                            <li><a tabindex="0">Something else here</a></li>
+                            <li class="dropdown-submenu"><a tabindex="0" data-toggle="dropdown">Another action</a>
+                                <ul class="dropdown-menu">
+                                    <li><a tabindex="0">Sub action</a></li>
+                                    <li><a tabindex="0">Another sub action</a></li>
+                                    <li><a tabindex="0">Something else here</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="dropdown-submenu"><a tabindex="0" data-toggle="dropdown">Another action</a>
+                        <ul class="dropdown-menu">
+                            <li><a tabindex="0">Sub action</a></li>
+                            <li><a tabindex="0">Another sub action</a></li>
+                            <li><a tabindex="0">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li><a tabindex="0">Something else here</a></li>
+                    <li class="divider"></li>
+                    <li><a tabindex="0">Separated link</a></li>
+                </ul>
+            </li>
+            <li class="dropdown"><a tabindex="0" data-toggle="dropdown">Dropdown 2<span class="caret"></span></a>
+                <ul class="dropdown-menu" role="menu">
+                    <li class="dropdown-submenu"><a tabindex="0" data-toggle="dropdown">Action</a>
+                        <ul class="dropdown-menu">
+                            <li><a tabindex="0">Sub action</a></li>
+                            <li class="dropdown-submenu"><a tabindex="0" data-toggle="dropdown">Another sub
+                                    action</a>
+                                <ul class="dropdown-menu">
+                                    <li><a tabindex="0">Sub action</a></li>
+                                    <li><a tabindex="0">Another sub action</a></li>
+                                    <li><a tabindex="0">Something else here</a></li>
+                                </ul>
+                            </li>
+                            <li><a tabindex="0">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li><a tabindex="0">Another action</a></li>
+                    <li class="dropdown-submenu"><a tabindex="0" data-toggle="dropdown">Something else here</a>
+                        <ul class="dropdown-menu">
+                            <li><a tabindex="0">Sub action</a></li>
+                            <li><a tabindex="0">Another sub action</a></li>
+                            <li><a tabindex="0">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="divider"></li>
+                    <li><a tabindex="0">Separated link</a></li>
+                </ul>
+            </li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+            <li class="dropdown"><a tabindex="0" data-toggle="dropdown">Dropdown 3<span class="caret"></span></a>
+                <ul class="dropdown-menu" role="menu">
+                    <li><a tabindex="0">Action</a></li>
+                    <li><a tabindex="0">Another action</a></li>
+                    <li><a tabindex="0">Something else here</a></li>
+                    <li class="divider"></li>
+                    <li><a tabindex="0">Separated link</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</nav>
+-->
+
 <ul class="nav nav-pills">
 
     <li role="presentation" class="active"><a href="index.php">Accueil</a></li>
     <li role="presentation"><a href="staff.php">Qui sommes nous ?</a></li>
     <li role="presentation"><a href="blog.php">Le blog</a></li>
 
-    <li role="presentation" class="dropdown">
-        <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-expanded="false">
-            Dop <span class="caret"></span>
-        </a>
+
+    <li class="dropdown"><a tabindex="0" data-toggle="dropdown">Dropdown test<span class="caret"></span></a>
         <ul class="dropdown-menu" role="menu">
-            <li>ititi</li>
-            <li>toto</li>
+            <li class="dropdown-submenu"><a tabindex="0" data-toggle="dropdown">Action</a>
+                <ul class="dropdown-menu">
+                    <li><a tabindex="0">Sub action</a></li>
+                    <li class="dropdown-submenu"><a tabindex="0" data-toggle="dropdown">Another sub
+                            action</a>
+                        <ul class="dropdown-menu">
+                            <li><a tabindex="0">Sub action</a></li>
+                            <li><a tabindex="0">Another sub action</a></li>
+                            <li><a tabindex="0">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li><a tabindex="0">Something else here</a></li>
+                </ul>
+            </li>
+            <li><a tabindex="0">Another action</a></li>
+            <li class="dropdown-submenu"><a tabindex="0" data-toggle="dropdown">Something else here</a>
+                <ul class="dropdown-menu">
+                    <li><a tabindex="0">Sub action</a></li>
+                    <li><a tabindex="0">Another sub action</a></li>
+                    <li><a tabindex="0">Something else here</a></li>
+                </ul>
+            </li>
+            <li class="divider"></li>
+            <li><a tabindex="0">Separated link</a></li>
         </ul>
     </li>
-
     <li role="presentation"><a href="emploi.php">Offre d'emploi</a></li>
     <li role="presentation"><a href="juridiction.php">Juridiction</a></li>
     <li role="presentation"><a href="locaux.php">Locaux</a></li>
